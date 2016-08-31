32885054873ffa06b2db8fa1751f932895476cb6
Chris Gmyr <cmgmyr@gmail.com>
2016-08-31 16:29:18 -0400

    added unread message count to each thread

--- resources/views/messenger/index.blade.php
+++ resources/views/messenger/index.blade.php
@@ -10,7 +10,7 @@
         @foreach($threads as $thread)
         <?php $class = $thread->isUnread($currentUserId) ? 'alert-info' : ''; ?>
         <div class="media alert {{ $class }}">
-            <h4 class="media-heading">{!! link_to('messages/' . $thread->id, $thread->subject) !!}</h4>
+            <h4 class="media-heading">{!! link_to('messages/' . $thread->id, $thread->subject) !!} ({{ $thread->userUnreadMessagesCount(Auth::id()) }} unread)</h4>
             <p>{{ $thread->latestMessage->body }}</p>
             <p><small><strong>Creator:</strong> {{ $thread->creator()->name }}</small></p>
             <p><small><strong>Participants:</strong> {{ $thread->participantsString(Auth::id()) }}</small></p>
